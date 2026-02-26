d0ecc06779a8441943065ee5491e3d2754d1b6fc
Savvydon <donsave4real@gmail.com>
2026-02-26 20:43:06 +0100

    Updated work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,48 @@
-// import { Routes, Route } from "react-router-dom";
-// import PhysicalFitness from "./components/PhysicalFitness.jsx";
-// import Results from "./components/Results.jsx";
-
-// export default function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<PhysicalFitness />} />
-//       <Route path="/results" element={<Results />} />
-//     </Routes>
-//   );
-// }
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PhysicalFitness from "./components/PhysicalFitness.jsx";
 import Results from "./components/Results.jsx";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter basename="/PhysicalFitness">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<PhysicalFitness />} />
+        <Route path="/PhysicalFitness" element={<PhysicalFitness />} />
         <Route path="/results" element={<Results />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
+
+
+
+// // import { Routes, Route } from "react-router-dom";
+// // import PhysicalFitness from "./components/PhysicalFitness.jsx";
+// // import Results from "./components/Results.jsx";
+
+// // export default function App() {
+// //   return (
+// //     <Routes>
+// //       <Route path="/" element={<PhysicalFitness />} />
+// //       <Route path="/results" element={<Results />} />
+// //     </Routes>
+// //   );
+// // }
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import PhysicalFitness from "./components/PhysicalFitness.jsx";
+// import Results from "./components/Results.jsx";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter basename="/PhysicalFitness">
+//       <Routes>
+//         <Route path="/" element={<PhysicalFitness />} />
+//         <Route path="/results" element={<Results />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
