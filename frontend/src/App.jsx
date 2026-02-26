13914b386dfc12d12cf6b46c64da955d3d674e70
Savvydon <donsave4real@gmail.com>
2026-02-26 21:38:29 +0100

    Updated work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,67 +1,17 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import PhysicalFitness from "./components/PhysicalFitness.jsx";
-// import Results from "./components/Results.jsx";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<PhysicalFitness />} />
-//         <Route path="/PhysicalFitness" element={<PhysicalFitness />} />
-//         <Route path="/results" element={<Results />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
-
-// import { Routes, Route } from "react-router-dom";
-// import PhysicalFitness from "./components/PhysicalFitness.jsx";
-// import Results from "./components/Results.jsx";
-
-// export default function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<PhysicalFitness />} />
-//       <Route path="/results" element={<Results />} />
-//     </Routes>
-//   );
-// }
-
-// // import { BrowserRouter, Routes, Route } from "react-router-dom";
-// // import PhysicalFitness from "./components/PhysicalFitness.jsx";
-// // import Results from "./components/Results.jsx";
-
-// // export default function App() {
-// //   return (
-// //     <BrowserRouter basename="/PhysicalFitness">
-// //       <Routes>
-// //         <Route path="/" element={<PhysicalFitness />} />
-// //         <Route path="/results" element={<Results />} />
-// //       </Routes>
-// //     </BrowserRouter>
-// //   );
-// // }
-
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PhysicalFitness from "./components/PhysicalFitness.jsx";
 import Results from "./components/Results.jsx";
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      {/* Redirect root to /PhysicalFitness */}
-      <Route path="/" element={<Navigate to="/PhysicalFitness" replace />} />
-
-      {/* Main pages */}
-      <Route path="/PhysicalFitness" element={<PhysicalFitness />} />
-      <Route path="/PhysicalFitness/results" element={<Results />} />
-
-      {/* Catch-all: redirect any unknown routes to home */}
-      <Route path="*" element={<Navigate to="/PhysicalFitness" replace />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PhysicalFitness />} />
+        <Route path="/PhysicalFitness" element={<PhysicalFitness />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,9 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter basename="/PhysicalFitness">
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-// Set basename for production deployment
-const basename = "/PhysicalFitness";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
