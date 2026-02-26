d78a92ff9b88659f6baf172755fe448c68c96100
Savvydon <donsave4real@gmail.com>
2026-02-26 20:30:06 +0100

    Updated work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+// import PhysicalFitness from "./components/PhysicalFitness.jsx";
+// import Results from "./components/Results.jsx";
+
+// export default function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<PhysicalFitness />} />
+//       <Route path="/results" element={<Results />} />
+//     </Routes>
+//   );
+// }
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PhysicalFitness from "./components/PhysicalFitness.jsx";
 import Results from "./components/Results.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PhysicalFitness />} />
-      <Route path="/results" element={<Results />} />
-    </Routes>
+    <BrowserRouter basename="/PhysicalFitness">
+      <Routes>
+        <Route path="/" element={<PhysicalFitness />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
