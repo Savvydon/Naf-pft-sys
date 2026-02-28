0f86b0ddd0447f8dfe41b76cc03173a789fe29f6
Oladan-Grandpa <62756952+oladoyindani@users.noreply.github.com>
2026-02-28 16:57:19 +0100

    Add files via upload

--- backend/app/services/database.py
+++ backend/app/services/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/naf_pft_db"
+DATABASE_URL = "mysql+pymysql://davetra2_oladoyin:GrandpaSage2@localhost/davetra2_naf_pft_sys"
 #use this below if you get password
 #DATABASE_URL = "mysql+pymysql://root:password@localhost/naf_pft_db"
 
